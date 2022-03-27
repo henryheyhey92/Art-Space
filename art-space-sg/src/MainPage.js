95c6d5bfa7350798d85998b686e7c23c869b8a6f
henrylai <e0032134@u.nus.edu>
2022-03-28 07:55:40 +0800

    first init

---
+++ art-space-sg/src/MainPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Main extends React.Component{
+    render(){
+        return (
+            <div className='container'>
+                <ul className='nav nav-tabs'>
+                    <li className='nav-item'>
+                            <button className='nav-link'>
+                                    testing
+                            </button>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
