4c6c996ed9888d03acdc6682fc76f59a3d93666f
henrylai <e0032134@u.nus.edu>
2022-04-08 21:20:50 +0800

    style create/edit page part 3

--- art-space-sg/src/Form.js
+++ art-space-sg/src/Form.js
@@ -14,6 +14,7 @@ import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import TextField from '@mui/material/TextField';
 import { FormGroup } from 'react-bootstrap';
+import { styled } from '@mui/material/styles';
 
 const sexSelection = [
     {
@@ -74,7 +75,7 @@ export default function Form(props) {
 
     return (
         <Box sx={{ m: 5 }}>
-            <Paper elevation={3}>
+            <Paper elevation={3} sx={{pb:1}}>
                 <Box
                     sx={{
                         m: 2,
@@ -84,13 +85,14 @@ export default function Form(props) {
                         minWdith: "sx"
                     }}
                 >
-                    <TextField id="name"
+                    <TextField id="url-name"
                         label="Image Url"
                         variant="outlined"
                         name='imageLinkName'
                         value={props.imageLink}
                         onChange={props.updateFormField}
                         className="textfield-image-url"
+                       
                     />
 
                     <TextField id="Artwork-name"
@@ -100,6 +102,7 @@ export default function Form(props) {
                         value={props.artWorkName}
                         onChange={props.updateFormField}
                         className="textfield-style"
+                        type="text"
                     />
 
                     <TextField
@@ -111,23 +114,24 @@ export default function Form(props) {
                         value={props.description}
                         onChange={props.updateFormField}
                         className="textfield-style"
+                        type="text"
                     />
                 </Box>
-                <Box sx={{m:2}}>
-                <Paper elevation={3} component="form"
-                    sx={{ p: '2px 4px', width: "100%" }}>
-                    <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+                <Box sx={{ m: 2 }}>
+                    <Paper elevation={3} component="form"
+                        sx={{ p: '2px 4px', width: "100%" }}>
+                        <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
 
-                        <FormLabel id="demo-radio-buttons-group-label">Category</FormLabel>
-                        <RadioGroup
-                            aria-labelledby="demo-radio-buttons-group-label"
-                            name="radio-buttons-group"
-                            sx={{ display: 'flex', flexDirection: 'row', justifyContent: "space-evenly" }}                            >
-                            {renderCatergoryOption()}
-                        </RadioGroup>
+                            <FormLabel id="demo-radio-buttons-group-label">Category</FormLabel>
+                            <RadioGroup
+                                aria-labelledby="demo-radio-buttons-group-label"
+                                name="radio-buttons-group"
+                                sx={{ display: 'flex', flexDirection: 'row', justifyContent: "space-evenly" }}                            >
+                                {renderCatergoryOption()}
+                            </RadioGroup>
 
-                    </FormControl>
-                </Paper>
+                        </FormControl>
+                    </Paper>
                 </Box>
                 <Box sx={{ m: 2 }}>
                     <Paper elevation={3}>
@@ -141,71 +145,79 @@ export default function Form(props) {
                 </Box>
                 {/* Artist particulars  */}
                 <Box
-                    component="form"
                     sx={{
-                        '& > :not(style)': { m: 2, width: '25ch' },
+                        m: 2,
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        minWdith: "sx"
                     }}
-                    noValidate
-                    autoComplete="off"
                 >
-                    <Paper elevation={3}>
-                        <label>
-                            Artist name:
-                            <input className="textfieldbox"
-                                type='text'
-                                name="artistName"
-                                value={props.artistName}
-                                onChange={props.updateFormField} />
-                        </label>
-                        {/* need the select dropdown */}
-                        <FormControl>
-                            <InputLabel id="demo-simple-select-label">Sex</InputLabel>
-                            <Select
-                                value={props.sex}
-                                label="sex"
-                                name="sex"
-                                onChange={props.updateFormField}
-                            >
-                                {renderSex()}
-                            </Select>
-                        </FormControl>
+                    <TextField id="artist-name"
+                        label="Artist name"
+                        variant="outlined"
+                        name='artistName'
+                        value={props.artistName}
+                        onChange={props.updateFormField}
+                        className="textfield-style"
+                        type="text"
+                    />
+                    <FormControl>
+                        <InputLabel id="demo-simple-select-label">Sex</InputLabel>
+                        <Select
+                            value={props.sex}
+                            label="sex"
+                            name="sex"
+                            onChange={props.updateFormField}
+                        >
+                            {renderSex()}
+                        </Select>
+                    </FormControl>
 
-                        <label>
-                            contact number:
-                            <input className="textfieldbox"
-                                type='text'
-                                name="contact"
-                                value={props.contactNum}
-                                onChange={props.updateFormField} />
-                        </label>
-                        <label>
-                            Password:
-                            <input className="textfieldbox"
-                                type='text'
-                                name="password"
-                                value={props.password}
-                                onChange={props.updateFormField} />
-                        </label>
-                        <label>
-                            Email:
-                            <input className="textfieldbox"
-                                type='text'
-                                name="email"
-                                value={props.email}
-                                onChange={props.updateFormField} />
-                        </label>
-                        <label>
-                            Price:
-                            <input className="textfieldbox"
-                                type='text'
-                                name="price"
-                                value={props.price}
-                                onChange={props.updateFormField} />
-                        </label>
-                    </Paper>
+                    <TextField id="contact-number"
+                        label="contact number"
+                        variant="outlined"
+                        name='contact'
+                        value={props.contactNum}
+                        onChange={props.updateFormField}
+                        className="textfield-style"
+                        type="number"
+                        inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                    />
+                     <TextField id="create-edit-password"
+                        label="password"
+                        variant="outlined"
+                        name='password'
+                        value={props.password}
+                        onChange={props.updateFormField}
+                        className="textfield-style"
+                        type="password"
+                    />
+                     <TextField id="create-edit-email"
+                        label="email"
+                        variant="outlined"
+                        name='email'
+                        value={props.email}
+                        onChange={props.updateFormField}
+                        className="textfield-style"
+                        type="text"
+                    />
+                     <TextField id="create-price"
+                        label="price"
+                        variant="outlined"
+                        name='price'
+                        value={props.price}
+                        onChange={props.updateFormField}
+                        className="textfield-style"
+                        type="number"
+                    />
                 </Box>
 
-                <Stack spacing={2} direction="row">
+                <Stack  
+                        direction="row" 
+                        justifyContent="space-around" 
+                        alignItems="center"
+                        m={2}>
                     <div>
                         {
                             !props.editBtn ?
